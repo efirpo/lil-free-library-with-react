16f46557a4c7e6069716d88da4128fbaf73623ee
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-21 16:40:57 -0700

    finalize changes for the day

--- library-react/src/components/BookDetail.js
+++ library-react/src/components/BookDetail.js
@@ -9,15 +9,36 @@ function BookDetail(props) {
   const showOtherDetails = async (id) => {
     let fetchString = `?bookId=` + id
     dispatch(makeJoinApiCall(fetchString)).then(() => { makeJoins(props.joins) }).then(() => {
+      console.log("These just post state slice:")
       console.log(theseJoins)
+
     })
+    grabJoinsFromLocations()
   }
+
+  let locationsArr;
+
   const grabJoinsFromLocations = () => {
+    let joinsIdArr = () => {
+      for (let i = 0; i <= theseJoins.joins.length; i++) {
+        return theseJoins.joins[i].locationId
+      }
+    }
+    locationsArr = props.locations.locations.filter((e) => {
+      for (let i = 0; i <= joinsIdArr.length; i++) {
+        if (e.locationId == joinsIdArr[i]) {
+          return e
+        }
+      }
+    })
 
   }
   let joinsTest;
   if (theseJoins) {
+    console.table("Join Table")
     console.table(theseJoins)
+    console.table("ID")
+
   }
   return (
     <React.Fragment>
